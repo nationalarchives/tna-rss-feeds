e0326e6c7ce0e64203afcbb522f632f4cdd56d57
Chris Bishop <domingobishop@gmail.com>
2015-12-30 15:26:52 +0000

    reduced number of items

--- tna-rss-feeds.php
+++ tna-rss-feeds.php
@@ -34,7 +34,7 @@ function rss_transient_func( $atts ){
 		if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
 			// Figure out how many total items there are and then limit it.
-			$maxitems = $rss->get_item_quantity( 6 );
+			$maxitems = $rss->get_item_quantity( 3 );
 
 			// Build an array of all the items, starting with element 0 (first element).
 			$rss_items = $rss->get_items( 0, $maxitems );
