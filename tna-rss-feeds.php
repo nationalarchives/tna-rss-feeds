38ec0770e697f8fb6deb9b7464e900cbd8a5c2b9
Chris Bishop <domingobishop@gmail.com>
2016-01-08 14:26:26 +0000

    changed transient to one hour

--- tna-rss-feeds.php
+++ tna-rss-feeds.php
@@ -113,7 +113,7 @@ function tna_rss( $rssUrl, $url, $rssTitle, $image, $id ) {
 				$html .= '</div>';
 				$n ++;
 			endforeach;
-			set_transient( 'tna_rss_blog_transient' . $id, $html, 6 * HOUR_IN_SECONDS );
+			set_transient( 'tna_rss_blog_transient' . $id, $html, HOUR_IN_SECONDS );
 			echo $html;
 		}
 		else {
